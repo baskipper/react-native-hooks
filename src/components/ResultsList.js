2bbc566728a33d121be78364ec293ec38d787f59
bskipper <baskipper67@gmail.com>
2019-09-24 10:35:07 -0500

    Created Image list of search results

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import {View, Text, StyleSheet, FlatList} from "react-native";
+import ResultsDetail from "./ResultsDetail";
 
 const ResultsList = ({title, results}) => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.title}>
                 {title}
             </Text>
             <FlatList
                 horizontal
+                showsHorizontalScrollIndicator={false}
                 data={results}
                 keyExtractor={result => result.id}
                 renderItem={({item}) => {
                     return (
-                        <Text>
-                            {item.name}
-                        </Text>
+                        <ResultsDetail
+                            result={item}
+                        />
                     )
                 }}
             />
@@ -24,9 +26,14 @@ const ResultsList = ({title, results}) => {
 };
 
 const styles = StyleSheet.create({
+    container: {
+        marginBottom: 10
+    },
     title: {
         fontSize: 18,
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        marginLeft: 15,
+        marginBottom: 5
     }
 });
 
